649371d2d47cf05ac857a289cc2ba428e73d5fdb
Topvennie <vincent@vallaeys.com>
2024-12-08 21:49:10 +0100

    chore(config): make multi threaded safe

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,14 +4,22 @@ package config
 import (
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
+// FIXME: Add mutex for map writes
+
+var mu sync.Mutex
+
 func bindEnv(key string) {
 	envName := strings.ToUpper(strings.ReplaceAll(key, ".", "_"))
-	// nolint:errcheck // we do not care if it can get binded
+
+	mu.Lock()
+	defer mu.Unlock()
+
 	viper.BindEnv(key, envName)
 }
 
@@ -20,11 +28,15 @@ func Init() error {
 	if err := godotenv.Load(); err != nil {
 		return err
 	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
 	viper.AutomaticEnv()
 	env := GetDefaultString("app.env", "development")
 
-	viper.SetConfigName(fmt.Sprintf("%s.toml", env))
-	viper.SetConfigType("toml")
+	viper.SetConfigName(fmt.Sprintf("%s.yaml", strings.ToLower(env)))
+	viper.SetConfigType("yaml")
 	viper.AddConfigPath("./config")
 
 	return viper.ReadInConfig()
@@ -33,11 +45,18 @@ func Init() error {
 // GetString returns the value of the key in string
 func GetString(key string) string {
 	bindEnv(key)
+
+	mu.Lock()
+	defer mu.Unlock()
+
 	return viper.GetString(key)
 }
 
 // GetDefaultString returns the value of the key in string or a default value
 func GetDefaultString(key, defaultValue string) string {
+	mu.Lock()
+	defer mu.Unlock()
+
 	viper.SetDefault(key, defaultValue)
 	return GetString(key)
 }
@@ -45,11 +64,18 @@ func GetDefaultString(key, defaultValue string) string {
 // GetStringSlice returns the value of the key in string slice
 func GetStringSlice(key string) []string {
 	bindEnv(key)
+
+	mu.Lock()
+	defer mu.Unlock()
+
 	return viper.GetStringSlice(key)
 }
 
 // GetDefaultStringSlice returns the value of the key in string slice or a default value
 func GetDefaultStringSlice(key string, defaultValue []string) []string {
+	mu.Lock()
+	defer mu.Unlock()
+
 	viper.SetDefault(key, defaultValue)
 	return GetStringSlice(key)
 }
@@ -57,11 +83,18 @@ func GetDefaultStringSlice(key string, defaultValue []string) []string {
 // GetInt returns the value of the key in int
 func GetInt(key string) int {
 	bindEnv(key)
+
+	mu.Lock()
+	defer mu.Unlock()
+
 	return viper.GetInt(key)
 }
 
 // GetDefaultInt returns the value of the key in int or a default value
 func GetDefaultInt(key string, defaultVal int) int {
+	mu.Lock()
+	defer mu.Unlock()
+
 	viper.SetDefault(key, defaultVal)
 	return GetInt(key)
 }
@@ -69,11 +102,18 @@ func GetDefaultInt(key string, defaultVal int) int {
 // GetUint16 returns the value of the key in uint16
 func GetUint16(key string) uint16 {
 	bindEnv(key)
+
+	mu.Lock()
+	defer mu.Unlock()
+
 	return viper.GetUint16(key)
 }
 
 // GetDefaultUint16 returns the value of the key in uint16 or a default value
 func GetDefaultUint16(key string, defaultVal uint16) uint16 {
+	mu.Lock()
+	defer mu.Unlock()
+
 	viper.SetDefault(key, defaultVal)
 	return GetUint16(key)
 }
@@ -81,11 +121,18 @@ func GetDefaultUint16(key string, defaultVal uint16) uint16 {
 // GetBool returns the value of the key in bool
 func GetBool(key string) bool {
 	bindEnv(key)
+
+	mu.Lock()
+	defer mu.Unlock()
+
 	return viper.GetBool(key)
 }
 
 // GetDefaultBool returns the value of the key in bool or a default value
 func GetDefaultBool(key string, defaultVal bool) bool {
+	mu.Lock()
+	defer mu.Unlock()
+
 	viper.SetDefault(key, defaultVal)
 	return GetBool(key)
 }
